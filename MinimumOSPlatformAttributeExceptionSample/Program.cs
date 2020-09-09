94aacba75aacddd7d9c90973b714bb389c0ee6ca
jiangzeng01 <49379637+jiangzeng01@users.noreply.github.com>
2020-09-09 02:24:17 -0700

    init
    init

---
+++ MinimumOSPlatformAttributeExceptionSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Versioning;
+
+namespace MinimumOSPlatformAttributeExceptionSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MinimumOSPlatformAttribute OSPlatform = new MinimumOSPlatformAttribute("win-x64");
+            Console.WriteLine(OSPlatform.PlatformName);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
